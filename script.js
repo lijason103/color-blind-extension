a11b0d115090ccda08c694c12ae4e96ef4dcd2a3
lijason103 <lijason103@gmail.com>
2018-12-01 15:09:28 -0800

    Added jquery Added color for all text

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // this is for the main page
 window.onload=function(){
-    console.log("page load!");
+    protanopes()
+}
 
-    console.log("aaa")
+
+function protanopes() {
+    var elements = $('*');
+
+    for(var i = 0; i < elements.length; i++) {
+       var current = elements[i];
+
+       if (current && current.style) {
+           current.style.color = 'green'
+        }
+
+
+        if(
+            (current.children.length === 0 && current.textContent.replace(/ |\n/g,'') !== '') 
+                || 
+            checkIfTextTag(current.nodeName)
+            ) {
+                current.style.backgroundColor = 'blue'
+        }
+    }
 }
