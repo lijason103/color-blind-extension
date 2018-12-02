054499e1996617a64928d84c40f6c8aeb8609c79
lijason103 <lijason103@gmail.com>
2018-12-01 16:33:31 -0800

    Added functionality to buttons

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,38 @@
-// This is for popup
-console.log("hello")
+const MODES = [
+    'acromatopsia',
+    'macular',
+    'protanope',
+    'deuteranope',
+    'tritanope',
+]
+
+for (let i = 0; i < MODES.length; ++i) {
+    document.getElementById(MODES[i]).addEventListener('click', () => {
+        storeMode(MODES[i])
+    })
+}
+
+function storeMode(mode) {
+    chrome.storage.sync.set({
+        mode,
+      }, () => {
+        alert("good")
+    });
+}
+
+var nextPageButton = document.getElementById('nextPage').addEventListener('click', () => {
+    document.getElementById('page-0').style.visibility = 'hidden'
+    document.getElementById('page-1').style.visibility = 'visible'
+})
+
+var returnPageButton = document.getElementById('backPage').addEventListener('click', () => {
+    document.getElementById('page-0').style.visibility = 'visible'
+    document.getElementById('page-1').style.visibility = 'hidden'
+})
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // this is for the main page
 window.onload=function(){
     protanopes()
+
+    chrome.storage.sync.get(['mode'], function(result) {
+        console.log('Value currently is ' + result.mode);
+      });
+    console.log("here")
 }
 
 
@@ -11,14 +16,14 @@ function protanopes() {
        var current = elements[i];
 
        if (current && current.style) {
-           current.style.color = 'green'
+           current.style.color = 'black'
         }
 
 
-        if(
-            (current.children.length === 0 && current.textContent.replace(/ |\n/g,'') !== '') 
-            ) {
-                current.style.backgroundColor = 'blue'
-        }
+        // if(
+        //     (current.children.length === 0 && current.textContent.replace(/ |\n/g,'') !== '') 
+        //     ) {
+        //         current.style.backgroundColor = 'blue'
+        // }
     }
 }
